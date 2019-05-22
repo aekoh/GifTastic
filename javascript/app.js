798daa3c1b06e3b02b766f6fa9fc00ae48aaee9b
aine <aineekohh@yahoo.com>
2019-05-22 18:55:18 -0500

    add

--- javascript/app.js
+++ javascript/app.js
@@ -1,48 +1,3 @@
-// Initial array of character
-var people = ["Arya Stark", "Tyrion Lannister", "khaleesi", "Jon Snow", "Joffrey Baratheon", "The hound", "Sansa Stark", "Khal Drogo"];
-
-// Function for displaying character data
-function renderButtons() {
-
-  // Deleting the character buttons prior to adding new movie buttons
-  // (this is necessary otherwise we will have repeat buttons)
-  $("#buttons-view").empty();
-
-  // Looping through the array of characters
-  for (var i = 0; i < people.length; i++) {
-
-    // Then dynamicaly generating buttons for each character in the array.
-    // This code $("<button>") is all jQuery needs to create the start and end tag. (<button></button>)
-    var a = $("<button>");
-
-    // Adding a class
-    a.addClass("peoples");
-    // Adding a data-attribute with a value of the character at index i
-    a.attr("data-name", people[i]);
-    // Providing the button's text with a value of the character at index i
-    a.text(people[i]);
-    // Adding the button to the HTML
-    $("#buttons-view").append(a);
-  }
-}
-// This function handles events where one button is clicked
-$("#add-people").on("click", function(event) {
-  // event.preventDefault() prevents the form from trying to submit itself.
-  // We're using a form so that the user can hit enter instead of clicking the button if they want
-  event.preventDefault();
-
-  // This line will grab the text from the input box
-  var peoples = $("#people-input").val().trim();
-  // The character from the textbox is then added to our array
-  people.push(peoples);
-
-  // calling renderButtons which handles the processing of our character array
-  renderButtons();
-});
-
-// Calling the renderButtons function at least once to display the initial list of characters
-renderButtons();
-
 
 // GIF edits
 
@@ -54,13 +9,11 @@ $(document).on("click", '.peoples', function() {
   // Constructing a queryURL using the animal name
 
   // CORS
-  var originalURL = 'https://api.giphy.com/v1/gifs/search?q=';
-  var queryURL = "https://cors-anywhere.herokuapp.com/" + originalURL
+  var originalURL = 'https://api.giphy.com/v1/gifs/search?q=' +
+  peopleSearch + "&api_key=dc6zaTOxFJmzC&limit=10";
 
 
-
-  var queryURL = 'https://api.giphy.com/v1/gifs/search?q=' +
-    peopleSearch + "&api_key=dc6zaTOxFJmzC&limit=10";
+  var queryURL = "https://cors-anywhere.herokuapp.com/" + originalURL;
   // Performing an AJAX request with the queryURL
   //example note
   $.ajax({
@@ -76,6 +29,7 @@ $(document).on("click", '.peoples', function() {
       console.log(queryURL);
       console.log('CORS anywhere response', response);
 
+    
 
       // storing the data from the AJAX request in the results variable
       var results = response.data;
@@ -123,5 +77,7 @@ $(document).on("click", ".result", function() {
         $(this).attr("data-state", "still");
     }
 });
+
+
 
 
